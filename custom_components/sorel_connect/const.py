d81ad887f0ea4f6f03c742d4a4f7aa6671efe2bb
Jaroslav Hanslík <kukulich@kukulich.cz>
2023-08-13 12:57:15 +0200

    Device info fixes

--- custom_components/sorel_connect/const.py
+++ custom_components/sorel_connect/const.py
@@ -1,5 +1,6 @@
 """SOREL Connect specific constants."""
 from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceEntryType
 import logging
 from typing import Final
 
@@ -8,12 +9,13 @@ LOGGER: Final = logging.getLogger(__package__)
 DOMAIN: Final = "sorel_connect"
 NAME: Final = "SOREL Connect"
 
-DEVICE_INFO: Final[DeviceInfo] = {
-	"identifiers": {(DOMAIN,)},
-	"model": NAME,
-	"default_name": NAME,
-	"manufacturer": NAME,
-}
+DEVICE_INFO: Final = DeviceInfo(
+	identifiers={(DOMAIN,)},
+	model=NAME,
+	name=NAME,
+	manufacturer=NAME,
+	entry_type=DeviceEntryType.SERVICE,
+)
 
 DATA_CLIENT: Final = "client"
 DATA_COORDINATOR: Final = "coordinator"
